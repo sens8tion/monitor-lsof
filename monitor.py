c71f1b5a5db0b4ee3463a00ed25f077cc35829ff
John Edwards <edwarj2@uk.ibm.com>
2020-06-10 10:43:17 +0100

    user update & error handling

--- monitor.py
+++ monitor.py
@@ -1,10 +1,21 @@
 import os
+import pwd
 
 def num_lsof_user(user):
-    stream = os.popen('lsof -u '+ user +' | wc -l')
-    output = stream.read()
-    return int(output)-1
+    if user == 'all':
+        stream = os.popen('lsof | wc -l')
+        output = stream.read()
+        return int(output)-1
+    else:
+        try:
+            pwd.getpwnam(user)
+        except KeyError:
+            print('User: ' + user + ' does not exist')
+            return -1
+        stream = os.popen('lsof -u '+ user +' | wc -l')
+        output = stream.read()
+        return int(output)-1
 
-users = ['root', 'johnedwards']
+users = ['root', 'homeassistant', 'all']
 for user in users:
     print(user + ": " + str(num_lsof_user(user)))
