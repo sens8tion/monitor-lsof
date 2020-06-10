96189b00b6023e63e2a2d3821010e8b04247ef48
John Edwards <edwarj2@uk.ibm.com>
2020-06-10 13:29:09 +0100

    minute pause & pre-commit frame

--- monitor.py
+++ monitor.py
@@ -1,26 +1,31 @@
 import os
 import pwd
 import time
+from datetime import datetime
 import json
 
+
 def num_lsof_user(user):
-    if user == 'all':
-        stream = os.popen('lsof | wc -l')
+    if user == "all":
+        stream = os.popen("lsof | wc -l")
         output = stream.read()
-        return int(output)-1
+        return int(output) - 1
     else:
         try:
             pwd.getpwnam(user)
         except KeyError:
             # print('User: ' + user + ' does not exist')
             return -1
-        stream = os.popen('lsof -u '+ user +' | wc -l')
+        stream = os.popen("lsof -u " + user + " | wc -l")
         output = stream.read()
-        return int(output)-1
+        return int(output) - 1
 
-users = ['all', 'root', 'johnedwards', 'homeassistant']
+
+users = ["all", "root", "homeassistant"]
 while True:
-    row = {"time":time.asctime()}
+    row = {"time": time.asctime()}
+    sleeptime = 60 - datetime.utcnow().second
+    time.sleep(sleeptime)
     for user in users:
         row[user] = num_lsof_user(user)
     print(json.dumps(row))
