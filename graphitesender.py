afbd3d76b18581b1332bbedb754903c3d144b3bd
John Edwards <edwarj2@uk.ibm.com>
2020-06-11 23:25:21 +0100

    graphite host!

---
+++ graphitesender.py
@@ -0,0 +1,33 @@
+import graphyte
+import os
+import pwd
+import time
+
+
+def num_lsof_user(user):
+    if user == "all":
+        stream = os.popen("lsof | wc -l")
+        output = stream.read()
+        return int(output) - 1
+    else:
+        try:
+            pwd.getpwnam(user)
+        except KeyError:
+            # print('User: ' + user + ' does not exist')
+            return -1
+        stream = os.popen("lsof -u " + user + " | wc -l")
+        output = stream.read()
+        return int(output) - 1
+
+
+usersList = ["all", "root", "homeassistant"]
+grafitehost = "edsoft.duckdns.org"
+
+graphyte.init(grafitehost, prefix="stats")
+
+while True:
+    time.sleep(4)
+    for user in usersList:
+        lsof = num_lsof_user(user)
+        print(f"{user} open files: {lsof}")
+        graphyte.send(user, num_lsof_user(user))
